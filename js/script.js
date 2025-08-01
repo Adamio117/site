473530c22c17a636a54ed1c0b6d18ccb20a0053e
Adamio117 <136378040+Adamio117@users.noreply.github.com>
2025-08-01 20:37:55 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -350,14 +350,14 @@
           .getElementById("showRegister")
           ?.addEventListener("click", (e) => {
             e.preventDefault();
-            document.getElementById("loginForm").style.display = "none";
-            document.getElementById("registerForm").style.display = "block";
+            document.getElementById("loginForm").classList.remove("show");
+            document.getElementById("registerForm").classList.add("show");
           });
 
         document.getElementById("showLogin")?.addEventListener("click", (e) => {
           e.preventDefault();
-          document.getElementById("registerForm").style.display = "none";
-          document.getElementById("loginForm").style.display = "block";
+          document.getElementById("registerForm").classList.remove("show");
+          document.getElementById("loginForm").classList.add("show");
         });
 
         // 3. Кнопка выхода
